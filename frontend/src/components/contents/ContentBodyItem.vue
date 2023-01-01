d83622ab8bf65c97dd9189c81677d996508f45ae
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-01-01 20:11:12 -0300

    features - frontend - components - ContentBodyItem: Sets the dynamic content body item component to data tables on views.

---
+++ frontend/src/components/contents/ContentBodyItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="content-body-item"
+  >
+    <h3>{{ value }}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ContentBodyItem',
+    props: {
+      value: [String, Number,  Date],
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/styles/views.module.scss';
+
+  .content-body-item {
+    width: 14%;
+
+    h3 {
+      margin-top: 5px;
+      margin-bottom: 5px;
+    }
+  }
+</style>
