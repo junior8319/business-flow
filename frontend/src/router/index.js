0d17d51badee71b30b34cddaad5c5e0052a8be52
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-01-01 20:08:39 -0300

    features - frontend - routes - /orders: Sets the route to Orders view.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Cnpjs.vue')
-  }
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+    component: () => import('../views/Orders.vue')
+  },
 ]
 
 const router = createRouter({
