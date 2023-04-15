72171bf9307c4ca8b12f6c9eb988631a04511b0d
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-04-15 12:09:20 -0300

    backend - feature - routes - Sponsors: Add sponsors to routes index.

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import cnpjsRouter from './cnpjs.routes';
 import ordersRouter from './orders.routes';
 import providersRouter from './providers.routes';
 import buyersRouter from './buyers.routes';
+import sponsorsRouter from './sponsor.routes';
 
 export {
   cnpjsRouter,
   ordersRouter,
   providersRouter,
   buyersRouter,
+  sponsorsRouter,
 };
